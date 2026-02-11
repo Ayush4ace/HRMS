fb15a6695097a0f1ead65c1457003a7ba9e599f4
Bookingcabs424 <neeraj@bookingcabs.com>
2026-02-11 13:16:26 +0530

    ADD: health route

--- app/main.py
+++ app/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 from app.routes import employee, attendance
+from fastapi.middleware.cors import CORSMiddleware
+
+
+origins = [
+    "http://localhost:5173",
+    "http://localhost:127.0.0.1:5173"
+]
 
 app = FastAPI(
     title="HRMS Lite API",
@@ -7,5 +14,18 @@ app = FastAPI(
     version="1.0.0"
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(employee.router)
 app.include_router(attendance.router)
+
+
+@app.get("/")
+def root():
+    return {"message": "HRMS API is running 🚀"}
